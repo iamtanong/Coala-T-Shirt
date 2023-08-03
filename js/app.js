4240411c6ba31968c2b386c1497670b380188d12
Tanongsak Anurak <tanongsak5913@gmail.com>
2023-08-03 12:06:03 +0700

    index

--- js/app.js
+++ js/app.js
@@ -1,7 +1,22 @@
-let user_icon_btn = document.querySelector(".user-icon-btn");
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false
+    },
 
-user_icon_btn.addEventListener("click", () => {
-    document.querySelector(".user-menu").classList.toggle("show")
-})
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
 
 
+});
---
+++ js/nav.js
@@ -0,0 +1,5 @@
+let user_icon_btn = document.querySelector(".user-icon-btn");
+
+user_icon_btn.addEventListener("click", () => {
+    document.querySelector(".user-menu").classList.toggle("show")
+})
